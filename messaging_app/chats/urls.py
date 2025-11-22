7b2b0ff0526bc4c849015cd3e891fe2556457e28
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-22 22:32:47 +0100

    correction

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
