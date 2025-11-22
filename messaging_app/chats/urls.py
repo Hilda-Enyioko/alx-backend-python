1a2135ea6609c62cbd00e9cf7b2074c882ff21fc
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-22 21:56:23 +0100

    adjusted default routers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,13 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 from django.contrib import admin
 from django.urls import path, include
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
-    router.urls,
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
 ]
