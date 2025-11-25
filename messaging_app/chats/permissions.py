09e0dd3911cae62c1481611b87304c24d61edb78
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-25 15:24:31 +0100

    added BasicAuthentication class

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,12 +1,12 @@
 # Permissions: Users can only access their conversations and messages.
-from rest_framework.permissions import BasePermissions
+from rest_framework import permissions
 
-class IsConversationParticipant(BasePermissions):
+class IsConversationParticipant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Assuming obj is a Conversation instance
         return request.user in obj.participants.all()
     
-class IsMessageOwner(BasePermissions):
+class IsMessageOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Assuming obj is a Message instance
         return request.user == obj.sender
