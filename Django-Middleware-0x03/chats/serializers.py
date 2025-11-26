f4f5e89c52867429312e8e2447335714cc75df66
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-26 14:05:14 +0100

    made a copy of the messaging_app directory in Django-Middleware-0x03

---
+++ Django-Middleware-0x03/chats/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Conversation, Message, User
+
+class UserSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.get_full_name', read_only=True)    
+
+    class Meta:
+        model = User
+        fields = '__all__'
+        read_only_fields = ['user_id', 'created_at', 'updated_at']
+        
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    sender_name = serializers.CharField(source='sender.get_full_name', read_only=True)
+    
+    class Meta:
+        model = Message
+        fields = '__all__'
+        read_only_fields = ['message_id', 'sent_at', 'sender_id']
+        
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+
+    messages = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = [
+            'conversation_id',
+            'participants',
+            'messages',
+            'created_at',
+        ]
+        read_only_fields = ['conversation_id', 'created_at']
+
+    def get_messages(self, obj):
+        msgs = obj.messages.all().order_by('sent_at')
+        return MessageSerializer(msgs, many=True).data
+
+    def validate(self, data):
+        participants = self.initial_data.get('participants', [])
+        if len(participants) < 2:
+            raise serializers.ValidationError("A conversation must include at least two participants.")
+        return data
