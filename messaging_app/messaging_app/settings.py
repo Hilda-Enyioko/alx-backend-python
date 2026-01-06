f697e44cf9219e3d3bc81bcccf1eddb948b10d7a
Hilda Enyioko <enyiokohilda@gmail.com>
2026-01-06 12:56:00 +0100

    Use github actions to automate testing for every pull and push

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
